542859e8d6bd114185e424dfe8e75c9640396012
MartinquaXD <martin.beckmann@protonmail.com>
2024-04-05 10:04:44 +0200

    Revert "Merge pull request #7 from gnosis/gas-estimate-offset"
    This reverts commit 1740937d98f07733699bd91bea9bf5f1337ddf86, reversing
    changes made to 6ae4fefbf0234f93226b7d785b1772681b366c51.

--- src/tests/oneinch/mod.rs
+++ src/tests/oneinch/mod.rs
@@ -16,16 +16,3 @@ exclude-liquidity = ['UNISWAP_V3', 'PMM4']
         ",
     ))
 }
-
-/// Creates a temporary file containing the config of the given solver and node.
-pub fn config_with_node(solver_addr: &SocketAddr, node: &SocketAddr) -> tests::Config {
-    tests::Config::String(format!(
-        r"
-node-url = 'http://{node}'
-[dex]
-chain-id = '1'
-endpoint = 'http://{solver_addr}'
-exclude-liquidity = ['UNISWAP_V3', 'PMM4']
-        ",
-    ))
-}
--- src/tests/zeroex/mod.rs
+++ src/tests/zeroex/mod.rs
@@ -5,21 +5,7 @@ mod not_found;
 mod options;
 mod out_of_price;
 
-/// Creates a temporary file containing the config of the given solver and node.
-pub fn config_with_node(solver_addr: &SocketAddr, node: &SocketAddr) -> tests::Config {
-    tests::Config::String(format!(
-        r"
-node-url = 'http://{node}'
-[dex]
-endpoint = 'http://{solver_addr}/swap/v1/'
-api-key = 'SUPER_SECRET_API_KEY'
-        ",
-    ))
-}
-
 /// Creates a temporary file containing the config of the given solver.
-/// Does not have access to a node so only suitable for tests that not rely on
-/// that.
 pub fn config(solver_addr: &SocketAddr) -> tests::Config {
     tests::Config::String(format!(
         r"
