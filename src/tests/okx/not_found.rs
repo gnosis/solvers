12bb1f1db6b4540a2b6c6e956208eefcc9ff9c76
MartinquaXD <martin.beckmann@protonmail.com>
2025-02-05 09:03:07 +0000

    cargo fmt

--- src/tests/okx/not_found.rs
+++ src/tests/okx/not_found.rs
@@ -14,7 +14,7 @@ async fn sell() {
              fromTokenAddress=0xc8cd2be653759aed7b0996315821aae71e1feadf&\
              toTokenAddress=0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2&slippage=0.01&\
              userWalletAddress=0x9008d19f58aabd9ed0d60971565aa8510560ab41&\
-             swapReceiverAddress=0x9008d19f58aabd9ed0d60971565aa8510560ab41"
+             swapReceiverAddress=0x9008d19f58aabd9ed0d60971565aa8510560ab41",
         ),
         res: json!({"code":"82000","data":[],"msg":"Insufficient liquidity."}),
     }])
