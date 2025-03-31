e4fd3244985c80304e84960b8db69b15673e092f
MartinquaXD <martin.beckmann@protonmail.com>
2025-03-31 07:06:31 +0000

    Clean up code slightly

--- src/infra/dex/balancer/batch_router.rs
+++ src/infra/dex/balancer/batch_router.rs
@@ -4,7 +4,7 @@
 
 use {
     crate::domain::{dex, eth},
-    contracts::BalancerV3BatchRouter,
+    contracts::{ethcontract::Bytes, BalancerV3BatchRouter},
     ethereum_types::{H160, U256},
 };
 
@@ -36,30 +36,10 @@ impl Router {
 
     pub fn swap_exact_amount_in(&self, paths: Vec<SwapPath>) -> dex::Call {
         let call = self.0.swap_exact_in(
-            paths
-                .into_iter()
-                .map(|p| {
-                    (
-                        p.token_in,
-                        p.steps
-                            .into_iter()
-                            .map(|s| (s.pool, s.token_out, s.is_buffer))
-                            .collect(),
-                        p.input_amount_raw,
-                        p.output_amount_raw,
-                    )
-                })
-                .collect(),
-            // `deadline`: Sufficiently large value with as many 0's as possible for some
-            // small gas savings.
-            U256::one() << 255,
-            // `wethIsEth`: If true, incoming ETH will be wrapped to WETH and
-            // outgoing WETH will be unwrapped to ETH. Since the settlement contract
-            // only works with WETH we don't have to think about wrapping.
-            false,
-            // user data which is by default empty in the
-            // balancer SDK.
-            Default::default(),
+            Self::encode_paths(paths),
+            Self::deadline(),
+            Self::weth_is_eth(),
+            Self::user_data(),
         );
 
         dex::Call {
@@ -70,35 +50,55 @@ impl Router {
 
     pub fn swap_exact_amount_out(&self, paths: Vec<SwapPath>) -> dex::Call {
         let call = self.0.swap_exact_out(
-            paths
-                .into_iter()
-                .map(|p| {
-                    (
-                        p.token_in,
-                        p.steps
-                            .into_iter()
-                            .map(|s| (s.pool, s.token_out, s.is_buffer))
-                            .collect(),
-                        p.input_amount_raw,
-                        p.output_amount_raw,
-                    )
-                })
-                .collect(),
-            // `deadline`: Sufficiently large value with as many 0's as possible for some
-            // small gas savings.
-            U256::one() << 255,
-            // `wethIsEth`: If true, incoming ETH will be wrapped to WETH and
-            // outgoing WETH will be unwrapped to ETH. Since the settlement contract
-            // only works with WETH we don't have to think about wrapping.
-            false,
-            // user data which is by default empty in the
-            // balancer SDK.
-            Default::default(),
+            Self::encode_paths(paths),
+            Self::deadline(),
+            Self::weth_is_eth(),
+            Self::user_data(),
         );
 
         dex::Call {
             to: self.address(),
             calldata: call.tx.data.unwrap().0,
         }
+    }
+
+    /// Converts rust struct with readable fields into tuple arguments used by
+    /// the smart contract bindings.
+    #[allow(clippy::type_complexity)]
+    fn encode_paths(paths: Vec<SwapPath>) -> Vec<(H160, Vec<(H160, H160, bool)>, U256, U256)> {
+        paths
+            .into_iter()
+            .map(|p| {
+                (
+                    p.token_in,
+                    p.steps
+                        .into_iter()
+                        .map(|s| (s.pool, s.token_out, s.is_buffer))
+                        .collect(),
+                    p.input_amount_raw,
+                    p.output_amount_raw,
+                )
+            })
+            .collect()
+    }
+
+    /// Returns a `deadline` value that is sufficiently large with as many 0's
+    /// as possible for some small gas savings.
+    fn deadline() -> U256 {
+        U256::one() << 255
+    }
+
+    /// Returns value for the `wethIsEth` argument. If that is true, incoming
+    /// ETH will be wrapped to WETH and outgoing WETH will be unwrapped to
+    /// ETH. Since the settlement contract only works with WETH we don't
+    /// have to think about wrapping.
+    fn weth_is_eth() -> bool {
+        false
+    }
+
+    /// Returns a value for the `userData` argument. The balancer SDK populates
+    /// that with an empty value so we are doing that as well.
+    fn user_data() -> Bytes<Vec<u8>> {
+        Default::default()
     }
 }
