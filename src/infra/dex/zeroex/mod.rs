8a9ad7c9a1411337fd55f0d73417d1a429b72dff
mrnaveira <47919901+mrnaveira@users.noreply.github.com>
2025-04-29 17:43:30 +0100

    map known zeroex error codes to NotFound

--- src/infra/dex/zeroex/mod.rs
+++ src/infra/dex/zeroex/mod.rs
@@ -166,13 +166,19 @@ impl From<util::http::RoundtripError<dto::Error>> for Error {
     fn from(err: util::http::RoundtripError<dto::Error>) -> Self {
         match err {
             util::http::RoundtripError::Http(err) => {
-                if let util::http::Error::Status(code, _) = err {
+                if let util::http::Error::Status(code, ref body) = err {
                     match code {
                         StatusCode::TOO_MANY_REQUESTS => Self::RateLimited,
                         StatusCode::UNAVAILABLE_FOR_LEGAL_REASONS => {
                             Self::UnavailableForLegalReasons
                         }
                         StatusCode::UNPROCESSABLE_ENTITY => Self::UnavailableForLegalReasons,
+                        // This seem to be a common Zeroex API response when not finding a trade
+                        StatusCode::BAD_REQUEST if body.contains("SWAP_VALIDATION_FAILED") => {
+                            Self::NotFound
+                        }
+                        // Also a common Zeroex API response when not finding a trade
+                        StatusCode::INTERNAL_SERVER_ERROR => Self::NotFound,
                         _ => Self::Http(err),
                     }
                 } else {
